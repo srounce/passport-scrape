e98f805d97d5b0e722537901e9e7c382903f42f3
Samuel Rounce <me@samuelrounce.co.uk>
2022-07-11 12:54:08 +0100

    Init

--- flake.nix
+++ flake.nix
@@ -2,21 +2,35 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system overlays };
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
         rustVersion = pkgs.rust-bin.stable.latest.default;
+
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = rustVersion;
+          rustc = rustVersion;
+        };
+
+        projectCrate = rustPlatform.buildRustPackage {
+          src = "./.";
+
+          cargoLock.lockFile = "./Cargo.lock";
+        };
       in {
         devShell = pkgs.mkShell {
           buildInputs = [
             (rustVersion.override {
-              extensions = [
-                "rust-src"
-              ];
+              extensions = [ "rust-src" ];
             })
+            pkgs.rust-analyzer
+            pkgs.openssl
+            pkgs.pkg-config
           ];
         };
       }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,64 @@
-fn main() {
-    println!("Hello, world!");
+use std::process;
+
+use tokio;
+use reqwest;
+use scraper::{Html, Selector};
+use notify_rust::Notification;
+
+const BUSY_MESSAGE: &str = "Sorry, there are no available appointments";
+
+enum ScrapeError {
+    Unknown(String),
+
+    InvalidBody,
+    InvalidPageSelector,
+
+    NotificationFailure(String),
+
+    AppointmentsUnavailable,
+}
+
+#[tokio::main]
+async fn main() {
+    match get_urgent().await {
+        Ok(_) => process::exit(0),
+        Err(_) => process::exit(1),
+    };
+}
+
+async fn get_urgent() -> Result<(), ScrapeError> {
+    let response = reqwest::get("https://www.passport.service.gov.uk/urgent")
+        .await
+        .map_err(|err| ScrapeError::Unknown(err.to_string()))?;
+
+    println!("Response status: {}", response.status());
+
+    let document = async {
+        let body = response.text()
+            .await
+            .map_err(|_| ScrapeError::InvalidBody)?;
+
+        Ok(Html::parse_document(&body))
+    }.await?;
+
+    let h1_selector = Selector::parse("h1")
+        .map_err(|_| ScrapeError::InvalidPageSelector)?;
+    
+    let heading_message = document
+        .select(&h1_selector)
+        .map(|heading| heading.text().collect::<String>())
+        .collect::<String>();
+
+    if heading_message == BUSY_MESSAGE {
+        return Err(ScrapeError::AppointmentsUnavailable)
+    }
+
+    Notification::new()
+        .summary("Passport appointments")
+        .body("Passport appointments available!")
+        .timeout(0)
+        .show()
+        .map_err(|err| ScrapeError::NotificationFailure(err.to_string()))?;
+
+    return Ok(())
 }
